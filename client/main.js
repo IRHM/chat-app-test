a9d9f02e404141d769e161d4caf664d219ba7fb1
IRHM <37304121+IRHM@users.noreply.github.com>
2020-10-10 13:19:42 +0100

    Basic chat server

---
+++ client/main.js
@@ -0,0 +1,22 @@
+var webSocket = new WebSocket("ws://192.168.0.11:8000/");
+
+document.getElementById("messageForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let msg = {
+    username: document.getElementById('usernameInput').value,
+    message: document.getElementById('messageInput').value
+  };
+
+  webSocket.send(JSON.stringify(msg));
+
+  console.log("hi");
+});
+
+webSocket.addEventListener('message', function(e) {
+  var msg = JSON.parse(e.data);
+
+  document.getElementById("messages").innerHTML += `
+    <strong>${msg.username}:</strong> ${msg.message} <br>
+  `;
+});
---
+++ server/messages.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+
+type Message struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+var clients = make(map[*websocket.Conn]bool) // connected clients
+var broadcast = make(chan Message) // broadcast channel
+var upgrader = websocket.Upgrader{}
+
+func startMessagesWebSocket() {
+	// Configure websocket route
+	http.HandleFunc("/", handleConnections)
+
+	// Start listening for incoming chat messages
+	go handleMessages()
+
+	// Start the server on localhost port 8000 and log any errors
+	log.Println("Server started on port 8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	// Allow every origin
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// Upgrade initial GET request to a websocket
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Make sure we close the connection when the function returns
+	defer ws.Close()
+
+	// Register our new client
+	clients[ws] = true
+
+	for {
+		var msg Message
+
+		// Read in a new message as JSON and map it to a Message object
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+
+		// Send the newly received message to the broadcast channel
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		// Grab the next message from the broadcast channel
+		msg := <-broadcast
+
+		// Send it out to every client that is currently connected
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
