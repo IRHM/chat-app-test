dabc6f5c3c65ad513a372f9f052ea2870e0023fd
IRHM <37304121+IRHM@users.noreply.github.com>
2020-10-11 16:05:00 +0100

    Notify user if connection to server succeeds or fails

--- client/main.js
+++ client/main.js
@@ -1,4 +1,34 @@
+/**
+ * Add a message to messages element locally
+ * @param {*} msg Message to add
+ */
+function notice(msg) {
+  document.getElementById("messages").innerHTML += `
+    <strong>${msg}</strong> <br>
+  `;
+}
+
 var webSocket = new WebSocket("wss://192.168.0.11:8000/");
+
+webSocket.addEventListener('open', (e) => {
+  if (WebSocket.OPEN) {  
+    notice("Connected");
+  }
+});
+
+webSocket.addEventListener('error', (e) => {
+  if (WebSocket.CLOSED) {
+    notice("Couldn't connect to server");
+  }
+});
+
+webSocket.addEventListener('message', (e) => {
+  var msg = JSON.parse(e.data);
+
+  document.getElementById("messages").innerHTML += `
+    <strong>${msg.username}:</strong> ${msg.body} <br>
+  `;
+});
 
 document.getElementById("messageForm").addEventListener("submit", (e) => {
   e.preventDefault();
@@ -12,10 +42,3 @@ document.getElementById("messageForm").addEventListener("submit", (e) => {
   }));
 });
 
-webSocket.addEventListener('message', function(e) {
-  var msg = JSON.parse(e.data);
-
-  document.getElementById("messages").innerHTML += `
-    <strong>${msg.username}:</strong> ${msg.body} <br>
-  `;
-});
